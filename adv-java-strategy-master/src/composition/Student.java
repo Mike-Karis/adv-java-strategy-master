f229d32c6879cb7389bf65baf18178eff861be61
Mike-Karis <77794142+Mike-Karis@users.noreply.github.com>
2022-05-05 12:39:19 -0500

    Add files via upload

---
+++ adv-java-strategy-master/src/composition/Student.java
@@ -0,0 +1,68 @@
+package composition;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private String streetAddress;
+    private int zipCode;
+    private String state;
+    private String phone;
+
+    public Student(String streetAddress, int zipCode, String state, String phone, String name, int age) {
+        this.streetAddress = streetAddress;
+        this.zipCode = zipCode;
+        this.state = state;
+        this.phone = phone;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public void setStreetAddress(String streetAddress) {
+        this.streetAddress = streetAddress;
+    }
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+}
